3880b28d6f29a3b099d78f02f39f231d8af03901
ankithans <ankithans1947@gmail.com>
2021-08-23 14:22:34 +0530

    feat: add broken registry commands

---
+++ pkg/cmd/registry/describe/describe.go
@@ -0,0 +1,116 @@
+package describe
+
+import (
+	"context"
+	"errors"
+
+	flagutil "github.com/aerogear/charmil-host-example/pkg/cmdutil/flags"
+	"github.com/aerogear/charmil-host-example/pkg/connection"
+	"github.com/aerogear/charmil-host-example/pkg/iostreams"
+	"github.com/aerogear/charmil-host-example/pkg/localize"
+	"github.com/aerogear/charmil-host-example/pkg/serviceregistry"
+
+	"github.com/aerogear/charmil-host-example/pkg/cmd/flag"
+
+	"github.com/aerogear/charmil-host-example/internal/config"
+	"github.com/aerogear/charmil-host-example/pkg/cmd/factory"
+	"github.com/aerogear/charmil-host-example/pkg/dump"
+	srsmgmtv1 "github.com/redhat-developer/app-services-sdk-go/registrymgmt/apiv1/client"
+	"github.com/spf13/cobra"
+)
+
+type Options struct {
+	id           string
+	name         string
+	outputFormat string
+
+	IO         *iostreams.IOStreams
+	Config     config.IConfig
+	Connection factory.ConnectionFunc
+	localizer  localize.Localizer
+}
+
+// NewDescribeCommand describes a service instance, either by passing an `--id flag`
+// or by using the service instance set in the config, if any
+func NewDescribeCommand(f *factory.Factory) *cobra.Command {
+	opts := &Options{
+		Config:     f.Config,
+		Connection: f.Connection,
+		IO:         f.IOStreams,
+		localizer:  f.Localizer,
+	}
+
+	cmd := &cobra.Command{
+		Use:     "describe",
+		Short:   f.Localizer.MustLocalize("registry.cmd.describe.shortDescription"),
+		Long:    f.Localizer.MustLocalize("registry.cmd.describe.longDescription"),
+		Example: f.Localizer.MustLocalize("registry.cmd.describe.example"),
+		Args:    cobra.RangeArgs(0, 1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			validOutputFormats := flagutil.ValidOutputFormats
+			if opts.outputFormat != "" && !flagutil.IsValidInput(opts.outputFormat, validOutputFormats...) {
+				return flag.InvalidValueError("output", opts.outputFormat, validOutputFormats...)
+			}
+
+			if len(args) > 0 {
+				opts.name = args[0]
+			}
+
+			if opts.name != "" && opts.id != "" {
+				return errors.New(opts.localizer.MustLocalize("service.error.idAndNameCannotBeUsed"))
+			}
+
+			if opts.id != "" || opts.name != "" {
+				return runDescribe(opts)
+			}
+
+			cfg, err := opts.Config.Load()
+			if err != nil {
+				return err
+			}
+
+			var registryConfig *config.ServiceRegistryConfig
+			if cfg.Services.ServiceRegistry == registryConfig || cfg.Services.ServiceRegistry.InstanceID == "" {
+				return errors.New(opts.localizer.MustLocalize("registry.common.error.noServiceSelected"))
+			}
+
+			opts.id = cfg.Services.ServiceRegistry.InstanceID
+
+			return runDescribe(opts)
+		},
+	}
+
+	cmd.Flags().StringVarP(&opts.outputFormat, "output", "o", "json", opts.localizer.MustLocalize("registry.cmd.flag.output.description"))
+	cmd.Flags().StringVar(&opts.id, "id", "", opts.localizer.MustLocalize("registry.common.flag.id"))
+
+	flagutil.EnableOutputFlagCompletion(cmd)
+
+	return cmd
+}
+
+func runDescribe(opts *Options) error {
+	connection, err := opts.Connection(connection.DefaultConfigSkipMasAuth)
+	if err != nil {
+		return err
+	}
+
+	api := connection.API()
+
+	var registry *srsmgmtv1.RegistryRest
+	ctx := context.Background()
+	if opts.name != "" {
+		registry, _, err = serviceregistry.GetServiceRegistryByName(ctx, api.ServiceRegistryMgmt(), opts.name)
+		if err != nil {
+			return err
+		}
+	} else {
+		registry, _, err = serviceregistry.GetServiceRegistryByID(ctx, api.ServiceRegistryMgmt(), opts.id)
+		if err != nil {
+			return err
+		}
+	}
+
+	dump.PrintDataInFormat(opts.outputFormat, registry, opts.IO.Out)
+
+	return nil
+}
---
+++ pkg/cmd/registry/use/use.go
@@ -0,0 +1,141 @@
+package use
+
+import (
+	"context"
+	"errors"
+
+	"github.com/aerogear/charmil-host-example/pkg/connection"
+	"github.com/aerogear/charmil-host-example/pkg/serviceregistry"
+	"github.com/aerogear/charmil/core/utils/iostreams"
+	"github.com/aerogear/charmil/core/utils/localize"
+
+	"github.com/aerogear/charmil-host-example/pkg/cmd/factory"
+	"github.com/aerogear/charmil-host-example/pkg/config"
+	"github.com/aerogear/charmil/core/utils/logging"
+	srsmgmtv1 "github.com/redhat-developer/app-services-sdk-go/registrymgmt/apiv1/client"
+	"github.com/spf13/cobra"
+)
+
+type Options struct {
+	id          string
+	name        string
+	interactive bool
+
+	IO         *iostreams.IOStreams
+	CfgHandler *config.CfgHandler
+	Connection factory.ConnectionFunc
+	Logger     func() (logging.Logger, error)
+	localizer  localize.Localizer
+}
+
+func NewUseCommand(f *factory.Factory) *cobra.Command {
+	opts := &Options{
+		CfgHandler: f.CfgHandler,
+		Connection: f.Connection,
+		Logger:     f.Logger,
+		IO:         f.IOStreams,
+		localizer:  f.Localizer,
+	}
+
+	cmd := &cobra.Command{
+		Use:     "use",
+		Short:   f.Localizer.LocalizeByID("registry.cmd.use.shortDescription"),
+		Long:    f.Localizer.LocalizeByID("registry.cmd.use.longDescription"),
+		Example: f.Localizer.LocalizeByID("registry.cmd.use.example"),
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if opts.id == "" && opts.name == "" {
+				if !opts.IO.CanPrompt() {
+					return errors.New(opts.localizer.LocalizeByID("registry.use.error.idOrNameRequired"))
+				}
+				opts.interactive = true
+			}
+
+			if opts.name != "" && opts.id != "" {
+				return errors.New(opts.localizer.LocalizeByID("service.error.idAndNameCannotBeUsed"))
+			}
+
+			return runUse(opts)
+		},
+	}
+
+	cmd.Flags().StringVar(&opts.id, "id", "", opts.localizer.LocalizeByID("registry.use.flag.id"))
+	cmd.Flags().StringVar(&opts.name, "name", "", opts.localizer.LocalizeByID("registry.use.flag.name"))
+
+	return cmd
+}
+
+func runUse(opts *Options) error {
+	if opts.interactive {
+		// run the use command interactively
+		err := runInteractivePrompt(opts)
+		if err != nil {
+			return err
+		}
+		// no service was selected, exit program
+		if opts.name == "" {
+			return nil
+		}
+	}
+
+	logger, err := opts.Logger()
+	if err != nil {
+		return err
+	}
+
+	connection, err := opts.Connection(connection.DefaultConfigSkipMasAuth)
+	if err != nil {
+		return err
+	}
+
+	api := connection.API()
+
+	var registry *srsmgmtv1.RegistryRest
+	ctx := context.Background()
+	if opts.name != "" {
+		registry, _, err = serviceregistry.GetServiceRegistryByName(ctx, api.ServiceRegistryMgmt(), opts.name)
+		if err != nil {
+			return err
+		}
+	} else {
+		registry, _, err = serviceregistry.GetServiceRegistryByID(ctx, api.ServiceRegistryMgmt(), opts.id)
+		if err != nil {
+			return err
+		}
+	}
+
+	registryConfig := &config.ServiceRegistryConfig{
+		InstanceID: registry.GetId(),
+		Name:       *registry.Name,
+	}
+
+	nameTmplEntry := localize.NewEntry("Name", registry.GetName())
+	opts.CfgHandler.Cfg.Services.ServiceRegistry = registryConfig
+
+	logger.Info(opts.localizer.LocalizeByID("registry.use.log.info.useSuccess", nameTmplEntry))
+
+	return nil
+}
+
+func runInteractivePrompt(opts *Options) error {
+	logger, err := opts.Logger()
+	if err != nil {
+		return err
+	}
+
+	connection, err := opts.Connection(connection.DefaultConfigSkipMasAuth)
+	if err != nil {
+		return err
+	}
+
+	logger.Info(opts.localizer.LocalizeByID("common.log.debug.startingInteractivePrompt"))
+
+	selectedRegistry, err := serviceregistry.InteractiveSelect(connection, logger)
+	if err != nil {
+		return err
+	}
+
+	opts.name = selectedRegistry.GetName()
+
+	return nil
+}
